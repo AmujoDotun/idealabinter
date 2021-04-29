33d4ffd43318b414cf3cbaec327be39173dd940d
AmujoDotun <samuelamujo@gmail.com>
2021-04-29 16:47:59 +0100

    created s3 bucket

--- database.tf
+++ database.tf
@@ -1,3 +1,29 @@
+# s3 buckek
+
+
+resource "aws_s3_bucket" "idealab" {
+  bucket = "idealabprod-for-db"
+  acl    = "private"
+
+  tags = {
+    Name        = "idealab bucket"
+    Environment = "idealabprod"
+  }
+}
+
+# for the load balancers logs
+
+resource "aws_s3_bucket" "idealablog" {
+  bucket = "idealabprodlog"
+  acl    = "public-read-write"
+
+  tags = {
+    Name        = "idealab bucket log"
+    Environment = "idealabprodlog"
+  }
+}
+
+
 resource "aws_rds_cluster_instance" "cluster_instances" {
   count              = 2
   identifier         = "aurora-cluster-demo-${count.index}"
